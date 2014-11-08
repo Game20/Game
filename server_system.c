068618ff9e02b0c8f15f153837dffc4c33d045d7
船坂　国之 <c501306014@clpc093.(none)>
2014-11-08 15:24:32 +0900

    CalcPos関数の追加

--- server_system.c
+++ server_system.c
@@ -8,3 +8,15 @@
 #include"server_common.h"
 #include"server_func.h"
 
+void CalcPos(int pos, int *intData){
+
+    gClients[pos].plc.x = RecvIntData(pos, &intData);
+    gClients[pos].plc.y = RecvIntData(pos, &intData);
+
+
+//HitJudge(
+        /*当たり判定*/
+
+
+}
+
