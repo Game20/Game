eb4431d114f9dfa2f3a845ce8026809e48cc2572
坪井　正夢 <c501306013@clpc065.(none)>
2014-11-14 13:23:23 +0900

    岩　完全版？

--- client_system.c
+++ client_system.c
@@ -137,9 +137,9 @@ void scroll(void){
     if(gameRect.x + shiftdef >= 0 && gameRect.x + shiftdef <= (MAP_Width - WIND_Width) * 60){
         gameRect.x += shiftdef;
         P.x -= shiftdef;
-		for(i=0; i<SUM_object+1; i++){
-			object[i].dst.x -= shiftdef;
-		}
+//		for(i=0; i<SUM_object+1; i++){
+//			object[i].dst.x -= shiftdef;
+//		}
     }
 }
 
