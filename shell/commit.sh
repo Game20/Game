596b6eb33d75f3a5646f4fac859f6220e87d07b1
高松　翔馬 <c501306016@clpc067.(none)>
2014-11-14 15:49:41 +0900

    rm -r shell

--- shell/commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Please Your Comment About Change"
-echo -n ">> "
-read comment
-
-git commit -m "${comment}"
-
-echo "Done Commit."
--- shell/reflesh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-rm -f *~
-echo "rm -f *~"
-rm -f *.o
-echo "rm -f *.o"
-
-
