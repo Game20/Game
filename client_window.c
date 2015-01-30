1e14e3792b81065903a6caeee278348b603456a2
坪井　正夢 <c501306013@clpc071.(none)>
2015-01-30 17:20:42 +0900

    リザルト画面製作中

--- client_window.c
+++ client_window.c
@@ -2,12 +2,12 @@
 //#include<SDL/SDL.h>
 #include "client_func.h"
 
-#define MSG_NUM 12           /* メッセージの数 */
+#define MSG_NUM 14           /* メッセージの数 */
 
 // static
 static char gFontFile[] = "images/APJapanesefontT.ttf";
 static char gMsgStrings[ 100 ][ 100 ] = { "さいしょから", "ステージ2から", "おわる", "つづける", "おわる",
-"1P", "2P", "3P", "4P", "Rabbit × Labyrinth", "ラビット×ラビリンス", "クリアタイム"};
+"1P", "2P", "3P", "4P", "Rabbit × Labyrinth", "ラビット×ラビリンス", "クリアタイム", "獲得トレジャーポイント", "1P---"};
 static SDL_Color black = {0x00, 0x00, 0x00};
 int setstartp;
 
@@ -17,7 +17,7 @@ TTF_Font* sTTF;
 SDL_Surface *gMessages[ 100 ];
 
 SDL_Surface *usa2, *neko, *inu, *panda;  // 画像データへのポインタ
-SDL_Surface *gameclear, *titleCG, *TFP, *jewels;
+SDL_Surface *gameclear, *titleCG, *TFP, *jewels, *usausa;
 
 char gMapDataFile[] = "map.data";
 char gMapDataFile2[] = "map2.data";
@@ -143,6 +143,8 @@ void InitWindow(){
 	TFP = IMG_Load("images/title@.jpg");
 
 	jewels = IMG_Load("images/jewel.png");
+
+	usausa = IMG_Load("images/usa-p-3.png");
 
 
     // フォントからメッセージ作成
@@ -1346,7 +1348,48 @@ gMessages[fm] = TTF_RenderUTF8_Blended(gTTF, gMsgStrings[fm], black);
         SDL_BlitSurface(gMessages[fm], &srcRect, SDL_GetVideoSurface(), &dstRect);
 
     SDL_Flip(window);// 画面に図形を表示（反映）
-    SDL_Delay(4000);
+    SDL_Delay(2000);
+
+fm = 12;
+        SDL_Rect srcRect = {0,0,0,0};
+        SDL_Rect dstRect = {150,700};
+        srcRect.w = gMessages[fm]->w;
+        srcRect.h = gMessages[fm]->h;
+        SDL_BlitSurface(gMessages[fm], &srcRect, SDL_GetVideoSurface(), &dstRect);
+
+    SDL_Flip(window);// 画面に図形を表示（反映）
+    SDL_Delay(2000);
+
+fm = 13;
+int jewelpoint1 = 0, jewelpoint2 = 0, jewelpoint3 = 0, jewelpoint4 = 0;
+ 
+while(1){
+
+sprintf (gMsgStrings[fm], "1P： %d  2P: %d  3P： %d  4P: %d", jewelpoint1, jewelpoint2, jewelpoint3, jewelpoint4);
+gMessages[fm] = TTF_RenderUTF8_Blended(gTTF, gMsgStrings[fm], black);
+        /* メッセージ表示 */
+        SDL_Rect srcRect = {0,0,0,0};
+        SDL_Rect dstRect = {100,600};
+        srcRect.w = gMessages[fm]->w;
+        srcRect.h = gMessages[fm]->h;
+        SDL_BlitSurface(gMessages[fm], &srcRect, SDL_GetVideoSurface(), &dstRect);
+    SDL_Flip(window);// 画面に図形を表示（反映）
+
+//if(jewelpoint1 < Player[0].jewelP)
+jewelpoint1++;
+
+jewelpoint2++;
+
+jewelpoint3++;
+
+jewelpoint4++;
+
+//if(jewelpoint1 == Player[0].jewelP && )
+
+
+}
+    SDL_Delay(2000);
+
 
     SDL_BlitSurface(TFP, NULL, window, NULL);
     SDL_Flip(window);// 画面に図形を表示（反映）
